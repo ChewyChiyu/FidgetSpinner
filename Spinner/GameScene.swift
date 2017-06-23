a9a081fa6f919a93b18cabcf42d00f738a3f030e
Evan Chen <evan@Evans-MacBook-Pro.local>
2017-06-23 12:44:03 -0700

    FIDGET SPINNNNER

--- Spinner/GameScene.swift
+++ Spinner/GameScene.swift
@@ -9,77 +9,125 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var spinner: SKSpriteNode!
+    
+    var centerLock: SKSpriteNode!
+    
+    var touchNode: SKSpriteNode!
+    
+    
+    var startTouch = CGPoint.zero
+    
+    var touchJoint: SKPhysicsJointSpring?
+
+    let userDefults = UserDefaults.standard
+    
+    //spins detection
+    var spinNode: SKLabelNode!
+    var hitSignal: SKSpriteNode!
+    
+    var spinTemp:UInt64 = 0{
+        didSet{
+            if(spinTemp%3==0){
+                spins+=1
+            }
+        }
+    }
+    
+    var spins:UInt64 = 0{
+        didSet{
+            spinNode!.text = "Spins : \(spins)"
+            userDefults.set(spins, forKey: "spins")
+        }
+    }
+    
     
     override func didMove(to view: SKView) {
+      
+        spinner = (self.childNode(withName: "Spinner") as! SKSpriteNode)
+        centerLock = (self.childNode(withName: "CenterPin") as! SKSpriteNode)
+        touchNode = (self.childNode(withName: "touchNode") as! SKSpriteNode)
+        spinNode = (self.childNode(withName: "SpinNode") as! SKLabelNode)
+        hitSignal = (self.childNode(withName: "hitDetector") as! SKSpriteNode)
+
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        physicsWorld.contactDelegate = self
+       
+        if let s = userDefults.value(forKey: "spins") {
+            spins = s as! UInt64
+        } else {
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
         }
+        
+        
+        addPin()
+        
+    }
+    func didBegin(_ contact: SKPhysicsContact) {
+        let pointA = contact.bodyA
+        let pointB = contact.bodyB
+        
+        if( pointA.node!.name == "Spinner" && pointB.node!.name == "hitDetector" || pointB.node!.name == "Spinner" && pointA.node!.name == "hitDetector"){
+            spinTemp+=1
+        }
+        
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    func addPin(){
+        
+        let centerPin = CGPoint(x:0,y:0)
+        let spinnerLock = SKPhysicsJointPin.joint(withBodyA: centerLock.physicsBody!, bodyB: spinner.physicsBody!, anchor: centerPin)
+        physicsWorld.add(spinnerLock)
+        
+               
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        //if touched before remove previous joint
+        if( touchJoint != nil ){
+            physicsWorld.remove(touchJoint!)
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        //set locations of touches
+        let touch = touches.first!
+        let recentTouch = touch.location(in: self)
+        startTouch = touch.location(in: self)
+
+        touchNode.position = recentTouch
+        
+        
+        
+        //seak if start was spinner
+        
+        if( atPoint(startTouch).name == "Spinner" ){
+            
+            //continue with code
+            touchJoint = SKPhysicsJointSpring.joint(withBodyA: touchNode.physicsBody!, bodyB: spinner.physicsBody!, anchorA: recentTouch, anchorB: startTouch)
+            physicsWorld.add(touchJoint!)
+            touchJoint!.frequency = 6
+            touchJoint!.damping = 0.5
+            
+        }
+        
+
+        
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        let touch = touches.first!
+        let recentTouch = touch.location(in: self)
+        touchNode.position = recentTouch
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        touchNode.position = centerLock.position
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+
     }
     
     
